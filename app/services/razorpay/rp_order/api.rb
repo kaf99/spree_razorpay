97d0256edebd7d2831d8ec5987289d738b96b917
Umesh Ravani <umeshravani98@gmail.com>
2025-08-04 23:11:06 +0530

    Update Razorpay::RpOrder::Api

--- app/services/razorpay/rp_order/api.rb
+++ app/services/razorpay/rp_order/api.rb
@@ -8,10 +8,14 @@ module Razorpay
 
         razorpay_order = Razorpay::Order.create(order_create_params)
 
-        return razorpay_order.id if razorpay_order.try(:id).present?
+        if razorpay_order.try(:id).present?
+          log_order_in_db(razorpay_order.id)
+          return razorpay_order.id
+        end
 
         ''
-      rescue StandardError
+      rescue StandardError => e
+        Rails.logger.error("Razorpay Order create failed: #{e.message}")
         ''
       end
 
@@ -19,10 +23,20 @@ module Razorpay
 
       def order_create_params
         {
-          "amount": order.inr_amt_in_paise,
-          "currency": order.currency,
-          "receipt": order.number
+          amount: order.inr_amt_in_paise,
+          currency: order.currency,
+          receipt: order.number
         }
+      end
+
+      def log_order_in_db(rzp_order_id)
+        Spree::RazorpayCheckout.create!(
+          order_id: order.id,
+          razorpay_order_id: rzp_order_id,
+          status: 'created'
+        )
+      rescue StandardError => e
+        Rails.logger.error("Failed to log Razorpay Order in DB: #{e.message}")
       end
     end
   end
