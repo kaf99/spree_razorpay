5853eaef7f7b64b54a6ff08bf858d13233804d75
Umesh Ravani <umeshravani98@gmail.com>
2025-08-07 01:58:59 +0530

    Create configuration.rb

---
+++ lib/spree_razorpay_checkout/configuration.rb
@@ -0,0 +1,10 @@
+module SpreeRazorpayCheckout
+  class Configuration
+    # Define settings here as needed, e.g.:
+    attr_accessor :some_setting
+
+    def initialize
+      @some_setting = 'default_value'
+    end
+  end
+end
