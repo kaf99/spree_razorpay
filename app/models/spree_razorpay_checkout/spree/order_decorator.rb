1b0b71f846392d66047f434d9ead65c1c7896c65
Umesh Ravani <umeshravani98@gmail.com>
2025-11-20 23:58:43 +0530

    Refactor razor_payment method to improve clarity

--- app/models/spree_razorpay_checkout/spree/order_decorator.rb
+++ app/models/spree_razorpay_checkout/spree/order_decorator.rb
@@ -5,26 +5,31 @@ module SpreeRazorpayCheckout
         (total.to_f * 100).to_i
       end
 
+      # Create a Spree::Payment and return it so controller can complete it
       def razor_payment(payment_object, payment_method, razorpay_signature)
-        payments.create!(
-          source: ::Spree::RazorpayCheckout.create!(
-            order_id: id,
-            razorpay_payment_id: payment_object.id,
-            razorpay_order_id: payment_object.order_id,
-            razorpay_signature: razorpay_signature,
-            status: payment_object.status,
-            payment_method: payment_object.method,
-            card_id: payment_object.card_id,
-            bank: payment_object.bank,
-            wallet: payment_object.wallet,
-            vpa: payment_object.vpa,
-            email: payment_object.email,
-            contact: payment_object.contact
-          ),
+        source = ::Spree::RazorpayCheckout.create!(
+          order_id: id,
+          razorpay_payment_id: payment_object.id,
+          razorpay_order_id: payment_object.order_id,
+          razorpay_signature: razorpay_signature,
+          status: payment_object.status,
+          payment_method: payment_object.method,
+          card_id: payment_object.card_id,
+          bank: payment_object.bank,
+          wallet: payment_object.wallet,
+          vpa: payment_object.vpa,
+          email: payment_object.email,
+          contact: payment_object.contact
+        )
+
+        payment = payments.create!(
+          source: source,
           payment_method: payment_method,
           amount: total,
-          response_code: payment_object.status
+          response_code: payment_object.id
         )
+
+        payment
       end
 
       ::Spree::Order.prepend SpreeRazorpayCheckout::Spree::OrderDecorator
