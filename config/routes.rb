67d6a6cd47f5355da34b88fe81542d94124b82d2
Umesh Ravani <umeshravani98@gmail.com>
2025-11-21 00:05:36 +0530

    Update Razorpay response route to accept POST and PATCH

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Spree::Core::Engine.add_routes do
-  # Add your extension routes here
+  
   post '/razorpay/create_order', to: 'razorpay#create_order'
-  post '/razorpay/response', to: 'razorpay#razor_response', as: :razorpay_response
+  #Accept both POST and PATCH
+  match '/razorpay/response', to: 'razorpay#razor_response', via: [:post, :patch], as: :razorpay_response
 end
