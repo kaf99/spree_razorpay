a476b01f9e3462d31a69697d25411a1a5e736342
kaf99 <this.noz99@gmail.com>
2025-11-23 14:08:20 +0530

    Update order_decorator.rb

--- app/models/spree_razorpay_checkout/spree/order_decorator.rb
+++ app/models/spree_razorpay_checkout/spree/order_decorator.rb
@@ -2,11 +2,12 @@ module SpreeRazorpayCheckout
   module Spree
     module OrderDecorator
 
+      # Convert total to paise (for Razorpay)
       def inr_amt_in_paise
         (total * 100).to_i
       end
 
-      # Create a Spree::Payment and return it so controller can complete it
+      # Create a Spree::Payment from Razorpay response
       def razor_payment(payment_object, payment_method, razorpay_signature)
         source = ::Spree::RazorpayCheckout.create!(
           order_id: id,
@@ -14,7 +15,6 @@ module SpreeRazorpayCheckout
           razorpay_order_id: payment_object.order_id,
           razorpay_signature: razorpay_signature,
           status: payment_object.status,
-          payment_method: payment_object.method,
           card_id: payment_object.card_id,
           bank: payment_object.bank,
           wallet: payment_object.wallet,
